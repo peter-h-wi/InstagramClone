1b78eb606fc64abd783386ceae4a9bec4da0bc6d
Peter Wi <peter.h.wi96@gmail.com>
2022-04-18 20:34:34 -0400

    Organized codes

--- InstagramRecreation/ContentView.swift
+++ InstagramRecreation/ContentView.swift
@@ -13,80 +13,97 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            HStack(alignment: .center) {
-                ProfileImage(imgName: "profile", width: 30)
-                Text("pita.parking")
-                    .bold()
-                Spacer()
-                Button {
-                    // show menu
-                } label: {
-                    Image(systemName: "ellipsis")
-                }
-                .foregroundColor(.primary)
-            }
-            .padding(.horizontal, 10)
+            header
+            
             Image("image")
                 .resizable()
                 .scaledToFit()
-            HStack {
-                Button {
-                    isLike.toggle()
-                } label: {
-                    Image(systemName: isLike ? "heart.fill" : "heart")
-                        .font(.title2)
-                        .foregroundColor(isLike ? .red : .primary)
-                }
-                Button {
-                    // show the comments page.
-                } label: {
-                    Image(systemName: "message")
-                        .font(.title2)
-                }
-                Button {
-                    // deliver it to some other people.
-                } label: {
-                    Image(systemName: "paperplane")
-                        .font(.title2)
-                }
-                Spacer()
-                Button {
-                    isBookmark.toggle()
-                } label: {
-                    Image(systemName: isBookmark ? "bookmark.fill" : "bookmark")
-                        .font(.title2)
-                }
+            buttons
+            likes
+            contentMessage
+        }
+    }
+    
+    private var header: some View {
+        HStack(alignment: .center) {
+            ProfileImage(imgName: "profile", width: 30)
+            Text("pita.parking")
+                .bold()
+            Spacer()
+            Button {
+                // show menu
+            } label: {
+                Image(systemName: "ellipsis")
             }
             .foregroundColor(.primary)
-            .padding(.horizontal, 10)
-            HStack {
-                ZStack {
-                    ProfileImage(imgName: "profile2", width: 20)
-                        .zIndex(3)
-                    ProfileImage(imgName: "profile3", width: 20)
-                        .offset(x: 13)
-                        .zIndex(2)
-                    ProfileImage(imgName: "profile4", width: 20)
-                        .offset(x: 26)
-                        .zIndex(1)
-                }
-                .padding(.trailing, 30)
-                
-                Text("Liked by **____sith** and **2 others**")
-            }
-            .padding(.horizontal, 10)
-            Group {
-                HStack {
-                    Text("pita.parking")
-                        .bold()
-                    Text("🌧 🌺 🍒")
-                }
-                Text("3 hours ago")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-            }
-            .padding(.horizontal, 10)
         }
+        .padding(.horizontal, 10)
+    }
+    
+    private var buttons: some View {
+        HStack {
+            Button {
+                isLike.toggle()
+            } label: {
+                Image(systemName: isLike ? "heart.fill" : "heart")
+                    .font(.title2)
+                    .foregroundColor(isLike ? .red : .primary)
+            }
+            Button {
+                // show the comments page.
+            } label: {
+                Image(systemName: "message")
+                    .font(.title2)
+            }
+            Button {
+                // deliver it to some other people.
+            } label: {
+                Image(systemName: "paperplane")
+                    .font(.title2)
+            }
+            Spacer()
+            Button {
+                isBookmark.toggle()
+            } label: {
+                Image(systemName: isBookmark ? "bookmark.fill" : "bookmark")
+                    .font(.title2)
+            }
+        }
+        .foregroundColor(.primary)
+        .padding(.horizontal, 10)
+    }
+    
+    private var likes: some View {
+        HStack {
+            ZStack {
+                ProfileImage(imgName: "profile2", width: 20)
+                    .zIndex(3)
+                ProfileImage(imgName: "profile3", width: 20)
+                    .offset(x: 13)
+                    .zIndex(2)
+                ProfileImage(imgName: "profile4", width: 20)
+                    .offset(x: 26)
+                    .zIndex(1)
+            }
+            .padding(.trailing, 30)
+            
+            Text("Liked by **____sith** and **2 others**")
+        }
+        .padding(.horizontal, 10)
+    }
+    
+    private var contentMessage: some View {
+        Group {
+            HStack {
+                Text("pita.parking")
+                    .bold()
+                Text("🌧 🌺 🍒")
+            }
+            Text("3 hours ago")
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+        .padding(.horizontal, 10)
     }
 }
 
@@ -101,6 +118,7 @@ struct ContentView_Previews: PreviewProvider {
 struct ProfileImage: View {
     let imgName: String
     let width: CGFloat
+    
     var body: some View {
         Image(imgName)
             .resizable()
